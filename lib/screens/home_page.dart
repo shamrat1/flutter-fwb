90f4f75d7a1eaf8e47cd5d8fab9f64e73fbfc0a6
mt-yasin <87004947+mt-yasin@users.noreply.github.com>
2021-11-09 16:25:36 +0600

    Product view in home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -17,11 +18,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentIndex = 0;
+  var homeCategories;
+  var products;
+
+  _loadData() async {
+    homeCategories = FirebaseFirestore.instance.collection("categories").where("show_in_home",isEqualTo: true).get();
+    var catIds = <String>[];
+    homeCategories.docs.forEach((element) {
+      catIds.add(element.id);
+    });
+    // // products = FirebaseFirestore.instance.collection("products").where("category.id",whereIn: catIds).get();
+    // Logger().e(products.size);
+    // Logger().w(catIds);
+  }
 
   @override
   Widget build(BuildContext context) {
-        
-
+    _loadData();
     return Scaffold(
       drawer: Drawer(
           child: ListView(
@@ -123,37 +136,125 @@ class _HomePageState extends State<HomePage> {
         return WishlistScreen();
 
       default:
-      return ListView(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text("Favourites"),
-              SizedBox(
-                height: 120,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: demoImages.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 100,
-                        margin: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            image: DecorationImage(
-                                image: NetworkImage(demoImages[index]),
-                                fit: BoxFit.cover)),
-                      );
-                    }),
-              ),
-            ],
-          ),
-          HomePageWidget('Clothing'),
-          HomePageWidget('Bakery'),
-          HomePageWidget('Skincare'),
-        ],
+
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        child: FutureBuilder<QuerySnapshot>(
+          future: homeCategories,
+          builder: (context, snapshot){
+            if(snapshot.connectionState == ConnectionState.waiting) return Center(child: CircularProgressIndicator(),);
+            var cats = snapshot.data!.docs;
+            return ListView.builder(
+              itemBuilder: (context, index){
+                var item = cats[index].data() as Map<String, dynamic>;
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(item["name"] ?? "No Name Found"),
+                        SizedBox(height: 10,),
+                        SizedBox(
+                          height: 120,
+                          child: FutureBuilder<QuerySnapshot>(
+                            future: FirebaseFirestore.instance.collection("products").where("category.id",isEqualTo: snapshot.data!.docs[index].id).get(),
+                            builder: (context, productSnapshot) {
+
+                              if(productSnapshot.connectionState == ConnectionState.waiting) return Center(child: CircularProgressIndicator(),);
+                              var products = productSnapshot.data!.docs;
+
+                              if(products.length == 0) return Container();
+
+                              return ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: products.length,
+                                  itemBuilder: (context, productIndex) {
+                                    var singleProduct = products[productIndex].data() as Map<String, dynamic>;
+                                    // return Container(
+                                    //   width: 100,
+                                    //   margin: EdgeInsets.all(5),
+                                    //   decoration: BoxDecoration(
+                                    //       borderRadius: BorderRadius.circular(20),
+                                    //       image: DecorationImage(
+                                    //           image: NetworkImage(demoImages[index]),
+                                    //           fit: BoxFit.cover)),
+                                    // );
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(20.0),
+                                          border: Border.all(),
+                                          image: DecorationImage(
+                                            image: NetworkImage(singleProduct["image"] ?? "https://via.placeholder.com/150"),
+                                            fit: BoxFit.fill
+                                          )
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Container(
+                                            height: 50,
+                                            width: 100,
+                                            decoration: BoxDecoration(
+                                              color: Colors.white,
+                                              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20.0), bottomRight: Radius.circular(20.0))
+                                            ),
+                                            child: Column(
+                                              
+                                              children: [
+                                                Text(singleProduct["name"] ?? "Product Name"),
+                                                Text((singleProduct["price"] ?? "" )+ " TK"),
+                                          
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  });
+                            }
+                          ),
+                        ),
+                      ],
+                    ),
+                );
+              },
+              itemCount: cats.length,
+            );
+          },
+        ),
       );
+      // return ListView(
+      //   children: [
+      //     Column(
+      //       crossAxisAlignment: CrossAxisAlignment.start,
+      //       mainAxisSize: MainAxisSize.min,
+      //       children: [
+      //         Text("Favourites"),
+      //         SizedBox(
+      //           height: 120,
+      //           child: ListView.builder(
+      //               scrollDirection: Axis.horizontal,
+      //               itemCount: demoImages.length,
+      //               itemBuilder: (context, index) {
+      //                 return Container(
+      //                   width: 100,
+      //                   margin: EdgeInsets.all(5),
+      //                   decoration: BoxDecoration(
+      //                       borderRadius: BorderRadius.circular(20),
+      //                       image: DecorationImage(
+      //                           image: NetworkImage(demoImages[index]),
+      //                           fit: BoxFit.cover)),
+      //                 );
+      //               }),
+      //         ),
+      //       ],
+      //     ),
+      //     HomePageWidget('Clothing'),
+      //     HomePageWidget('Bakery'),
+      //     HomePageWidget('Skincare'),
+      //   ],
+      // );
     }
   }
 }
